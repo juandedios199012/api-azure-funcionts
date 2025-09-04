e78e4b38d9b3f272dc589582a675de3a978e8ef4
Juan De Dios Baudazio <jbaudazio@gmail.com>
2025-09-03 23:51:04 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-import { app, input, output } from '@azure/functions';
-import namesHandler, { namesListInput, namesOutput } from './functions/names.js';
-import apellidosHandler, { apellidosListInput, apellidosOutput } from './functions/apellidos.js';
+import { app } from '@azure/functions';
 import testHandler from './functions/test.js';
 
-// Función de debug sin Cosmos DB
+// Solo función de test para diagnóstico
 app.http('test', {
   route: 'test',
   methods: ['GET', 'POST'],
   authLevel: 'anonymous'
 }, testHandler);
 
-// Registro del endpoint /api/names
-app.http('names', {
-  route: 'names',
-  methods: ['GET', 'POST'],
-  authLevel: 'anonymous',
-  extraInputs: [namesListInput],
-  extraOutputs: [namesOutput]
-}, namesHandler);
-
-// Registro del endpoint /api/apellidos (para diagnóstico)
-app.http('apellidos', {
-  route: 'apellidos',
-  methods: ['GET', 'POST'],
-  authLevel: 'anonymous',
-  extraInputs: [apellidosListInput],
-  extraOutputs: [apellidosOutput]
-}, apellidosHandler);
+// TODO: Agregar otras funciones después de solucionar el problema base
